7cae069a7a90a9e7d3ffdf6a50dac44e6457f1c9
REDDYPANNURU <raj.pannuru@gmil.com>
2026-04-01 09:42:58 +0530

    UC7 implementation

--- App/src/TrainManagementApp.java
+++ App/src/TrainManagementApp.java
@@ -1,35 +1,54 @@
 import java.util.*;
 
+class Bogie {
+    String bogieId;
+    int capacity;
+
+    Bogie(String bogieId, int capacity) {
+        this.bogieId = bogieId;
+        this.capacity = capacity;
+    }
+}
+
 class Train {
     String trainName;
-    HashMap<String, Integer> bogieCapacityMap;
+    List<Bogie> bogies;
 
     Train(String trainName) {
         this.trainName = trainName;
-        this.bogieCapacityMap = new HashMap<>();
+        this.bogies = new ArrayList<>();
     }
 
-    void addBogie(String bogieId, int capacity) {
-        bogieCapacityMap.put(bogieId, capacity);
+    void addBogie(String id, int capacity) {
+        bogies.add(new Bogie(id, capacity));
     }
 
-    void displayBogieCapacity() {
+    void sortByCapacity() {
+        Collections.sort(bogies, new Comparator<Bogie>() {
+            public int compare(Bogie b1, Bogie b2) {
+                return Integer.compare(b1.capacity, b2.capacity);
+            }
+        });
+    }
+
+    void display() {
         System.out.println("Train: " + trainName);
-        for (Map.Entry<String, Integer> entry : bogieCapacityMap.entrySet()) {
-            System.out.println(entry.getKey() + " - Capacity: " + entry.getValue());
+        for (Bogie b : bogies) {
+            System.out.println(b.bogieId + " - Capacity: " + b.capacity);
         }
     }
 }
 
 public class TrainApp {
     public static void main(String[] args) {
-        Train train = new Train("Express 606");
+        Train train = new Train("Express 707");
 
         train.addBogie("B1", 72);
         train.addBogie("B2", 64);
         train.addBogie("B3", 48);
         train.addBogie("B4", 90);
 
-        train.displayBogieCapacity();
+        train.sortByCapacity();
+        train.display();
     }
 }
